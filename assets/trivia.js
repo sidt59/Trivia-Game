1f0026eb99a9d6c187a96ee70604cb89af4f6d64
STS 59 <notippin@gmail.com>
2019-03-12 19:58:59 -0500

    holy shit...i actually got a basic quiz to work...now for the timer!

--- assets/trivia.js
+++ assets/trivia.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-// object that holds question/answers in various arrays
-// split options array into separate 
+// object that holds question/answers in various arrays 
 var triviaQuestions = [
     { 
       questions: "Who is the actor who plays Lando Calrissian?",
@@ -29,50 +28,22 @@ var triviaQuestions = [
     }, 
      
 ];
+// hide your shame
+$('#quiz').hide();
+$('#submit').hide();
+$('#reset').hide();
 
-
-
-// var to ref html div
-// var mainContent = $('#mainContent');
-
-// question index
-// var questionIndex = 0;
-
-// generate quiz.....again
-/* function generateQuiz() {
-    if (questionIndex <= (triviaQuestions.length - 1)) {
-        document.querySelector("#quiz").innerHTML = triviaQuestions[questionIndex].questions;
-        document.querySelector("#options").innerHTML = 
-        '<input type="radio" name="questionChoices" value="' + triviaQuestions[0].options[0] + '" checked="yes">' + triviaQuestions[0].options[0] + '</input>' +
-        '<input type="radio" name="questionChoices" value="' + triviaQuestions[0].options[1] + '" checked="yes">' + triviaQuestions[0].options[1] + '</input>' +
-        '<input type="radio" name="questionChoices" value="' + triviaQuestions[0].options[2] + '" checked="yes">' + triviaQuestions[0].options[2] + '</input>' +
-        '<input type="radio" name="questionChoices" value="' + triviaQuestions[0].options[3] + '" checked="yes">' + triviaQuestions[0].options[3] + '</input>';
-    } else {
-        docu3ent.querySelector('#quiz').innerHTML = "Game Over!";
-    }
-} */
-
-/* // references to containers in html
-var quizContainer = document.getElementById('quiz');
-var resultsContainer = document.getElementById('results');
-var submitButton = document.getElementById('submit'); */
-
-// generate quiz
-/* function generateQuiz(i) {
-    mainContent.html(
-        '<div id="questionDiv">' +
-        '<h1>Question ' + (i + 1) + '<h1>' +
-        '<h2>' + triviaQuestions[i].questions + '</h2>' +
-        '<input type="radio" name="questionChoices" value="' + triviaQuestions[i].options[0] + '" checked="yes">' + triviaQuestions[i].options[0] + '</input>' +
-        '<input type="radio" name="questionChoices" value="' + triviaQuestions[i].options[1] + '">' + triviaQuestions[i].options[1] + '</input>' +
-        '<input type="radio" name="questionChoices" value="' + triviaQuestions[i].options[2] + '">' + triviaQuestions[i].options[2] + '</input>' +
-        '<input type="radio" name="questionChoices" value="' + triviaQuestions[i].options[3] + '">' + triviaQuestions[i].options[3] + '</input>' +
-        '<button id="submitButton">Submit</button>' +
-        '</div>'
-    );
-} */
+// on start button -> generate quiz
+$('#start').on('click', function(){
+    generateQuiz();
+    $('#reset').show();
+    $('#submit').show();
+    $('#quiz').show();
+})
 
 function generateQuiz(){
+    $('#start').hide();
+    $('#quiz').show();
     $('#quiz').html(
         '<h3>' + triviaQuestions[0].questions + '</h3>' +
         '<input type="radio" name="question0" value="' + triviaQuestions[0].options[0] + '">' + triviaQuestions[0].options[0] + '</input>' +    
@@ -137,32 +108,57 @@ function generateQuiz(){
         console.log(totalScore);
         $('#quiz').hide();
         $('#results').html("You've gotten " + totalScore + " of 5 Correct!");
-        $('#submit').hide();
-        $('#reset').html(); // reset function here?
     });
 };
 
-generateQuiz();
+// reset button
+$('#reset').on('click', function () {
+    totalScore = 0;
+    generateQuiz();
+    $('#results').hide();
+})
 
-/* generateQuiz();
-// compare user choice to correct answer. record score
-function scoreCheck() {
-    if ($('input:radio[name=questionChoices]:checked').val() === triviaQuestions[0].answer) {
-        correctGuess++;
-        console.log(correctGuess);
+// doc.ready closing tag -- dont fuck with this!
+});
+
+
+
+// var to ref html div
+// var mainContent = $('#mainContent');
+
+// question index
+// var questionIndex = 0;
+
+// generate quiz.....again
+/* function generateQuiz() {
+    if (questionIndex <= (triviaQuestions.length - 1)) {
+        document.querySelector("#quiz").innerHTML = triviaQuestions[questionIndex].questions;
+        document.querySelector("#options").innerHTML = 
+        '<input type="radio" name="questionChoices" value="' + triviaQuestions[0].options[0] + '" checked="yes">' + triviaQuestions[0].options[0] + '</input>' +
+        '<input type="radio" name="questionChoices" value="' + triviaQuestions[0].options[1] + '" checked="yes">' + triviaQuestions[0].options[1] + '</input>' +
+        '<input type="radio" name="questionChoices" value="' + triviaQuestions[0].options[2] + '" checked="yes">' + triviaQuestions[0].options[2] + '</input>' +
+        '<input type="radio" name="questionChoices" value="' + triviaQuestions[0].options[3] + '" checked="yes">' + triviaQuestions[0].options[3] + '</input>';
     } else {
-        incorrectGuess++;
-        console.log(incorrectGuess);
+        docu3ent.querySelector('#quiz').innerHTML = "Game Over!";
     }
 } */
-    
 
+/* // references to containers in html
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit'); */
 
-/* // on submit then show score
-$('#submitButton').on('click', scoreCheck());
-console.log(scoreCheck());
-
- */
-
-// doc.ready closing tag. dont fuck with this
-});
+// generate quiz
+/* function generateQuiz(i) {
+    mainContent.html(
+        '<div id="questionDiv">' +
+        '<h1>Question ' + (i + 1) + '<h1>' +
+        '<h2>' + triviaQuestions[i].questions + '</h2>' +
+        '<input type="radio" name="questionChoices" value="' + triviaQuestions[i].options[0] + '" checked="yes">' + triviaQuestions[i].options[0] + '</input>' +
+        '<input type="radio" name="questionChoices" value="' + triviaQuestions[i].options[1] + '">' + triviaQuestions[i].options[1] + '</input>' +
+        '<input type="radio" name="questionChoices" value="' + triviaQuestions[i].options[2] + '">' + triviaQuestions[i].options[2] + '</input>' +
+        '<input type="radio" name="questionChoices" value="' + triviaQuestions[i].options[3] + '">' + triviaQuestions[i].options[3] + '</input>' +
+        '<button id="submitButton">Submit</button>' +
+        '</div>'
+    );
+} */
